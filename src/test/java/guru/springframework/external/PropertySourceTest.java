2c7993086d4d46b480e1f346dab6c0984384a3a2
Armen G <gharibi.armen@yahoo.com>
2017-05-08 20:55:09 -0700

    setting up test properties and code starter.

---
+++ src/test/java/guru/springframework/external/PropertySourceTest.java
@@ -0,0 +1,33 @@
+package guru.springframework.external;
+
+
+import guru.springframework.config.ExternalPropsPropertySourceTestConfig;
+import guru.springframework.jms.FakeJmsBroker;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = ExternalPropsPropertySourceTestConfig.class)
+public class PropertySourceTest {
+
+    private FakeJmsBroker fakeJmsBroker;
+
+    @Test
+    public void testPropSet() throws Exception {
+
+        assertEquals("10.10.10.123", fakeJmsBroker.getUrl());
+        assertEquals(3330, fakeJmsBroker.getPort().intValue());
+        assertEquals("Ron", fakeJmsBroker.getUser());
+        assertEquals("Burgundy", fakeJmsBroker.getPassword());
+    }
+
+    @Autowired
+    public void setFakeJmsBroker(FakeJmsBroker fakeJmsBroker) {
+        this.fakeJmsBroker = fakeJmsBroker;
+    }
+}
