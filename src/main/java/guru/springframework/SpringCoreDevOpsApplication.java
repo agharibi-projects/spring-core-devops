0cd07f43504a2dfedcd8acf9fb15008ec6d57963
Armen G <gharibi.armen@yahoo.com>
2017-07-18 20:59:32 -0700

    added github/jenkins webhook

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -9,8 +9,6 @@ public class SpringCoreDevOpsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
-		//this is a whitespace change test
-		// another test...
-		// Added chagnes for jenkins tests
+		// Added a comment to check the github/jenkins webhook
 	}
 }
